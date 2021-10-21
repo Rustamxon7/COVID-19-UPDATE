4a858ebfa6265e33aa87c1204bc274128344c37e
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-10-22 01:30:11 +0500

    Collect all Values in one container

---
+++ src/components/Home/Container.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Country from '../Country/Country';
+
+const Container = () => {
+  return (
+    <div className="app-container">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route>
+          <Country />
+        </Route>
+        <Route></Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default Container;
